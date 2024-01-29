7492bb918af40cef9b46058c622b14308f5f7dd1
Ender060 <derekpendley060@gmail.com>
2024-01-29 15:58:03 -0700

    Git Corection 0.1

--- OrionDown/Assets/Scripts/CameraPan.cs
+++ OrionDown/Assets/Scripts/CameraPan.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraPan : MonoBehaviour
 {
-        private Vector3 cameraRotation = new Vector3 (0,0,0);
+        private Vector3 cameraRotation = new Vector3 (0f,0f,0f);
         [SerializeField] private float xSensitivity = 0.5f;
         [SerializeField] private float ySensitivity = 0.5f;
     // Start is called before the first frame update
@@ -17,11 +17,11 @@ public class CameraPan : MonoBehaviour
     {
         cameraRotation.x -= xSensitivity * Input.GetAxis("Vertical");
         cameraRotation.y += ySensitivity * Input.GetAxis("Horizontal");
-        if (cameraRotation.x < -90) {
-            cameraRotation.x = -90;
+        if (cameraRotation.x < -90f) {
+            cameraRotation.x = -90f;
         }
-        if (cameraRotation.x > 90) {
-            cameraRotation.x = 90;
+        if (cameraRotation.x > 90f) {
+            cameraRotation.x = 90f;
         }
         transform.rotation = Quaternion.Euler(cameraRotation);
     }
