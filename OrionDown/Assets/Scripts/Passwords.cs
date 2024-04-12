fc3aa2f74a4a5aaee4c13b3361356b5418956861
qazxwecvr <lucasdangeranderson@gmail.com>
2024-04-12 09:52:44 -0600

    Simplify passwords code

--- OrionDown/Assets/Scripts/Passwords.cs
+++ OrionDown/Assets/Scripts/Passwords.cs
@@ -5,134 +5,49 @@ using TMPro;
 
 public class Passwords : MonoBehaviour
 {
-    public TMP_Text char1Text;
-    public TMP_Text char2Text;
-    public TMP_Text char3Text;
-    public TMP_Text char4Text;
-    public TMP_Text char5Text;
-    List<char> pos1 = new List<char>{'A','B','C'};
+    [SerializeField]
+    public TMP_Text[] charDisplays;
 
-    List<char> pos2 = new List<char>{'A','B','C'};
+    private char[][] characters = {
+        new char[] { 'A', 'B', 'C' },
+        new char[] { 'A', 'B', 'C' },
+        new char[] { 'A', 'B', 'C' },
+        new char[] { 'A', 'B', 'C' },
+        new char[] { 'A', 'B', 'C' }
+    };
 
-    List<char> pos3 = new List<char>{'A','B','C'};
+    private int[] currentIndices = new int[5];
 
-    List<char> pos4 = new List<char>{'A','B','C'};
+    private int[] solutionIndices = new int[5];
 
-    List<char> pos5 = new List<char>{'A','B','C'};
-
-    int c1 = 0;
-    int c2 = 0;
-    int c3 = 0;
-    int c4 = 0;
-    int c5 = 0;
-    List<int> solution = new List<int>();
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        for (int i = 0; i < characters.Length; i++)
+        {
+            charDisplays[i].text = characters[i][currentIndices[i]].ToString();
+        }
     }
+
     // Update is called once per frame
     void Update(){
-        
-
-        Debug.Log(pos1[c1].ToString());
-        Debug.Log(c1.ToString());
+       
+        Debug.Log(characters[0][currentIndices[0]].ToString());
+        Debug.Log(currentIndices[0].ToString());
     }
     public void Cycle(int buttonPressed)
     {
-       if (buttonPressed == 0){
-            if (c1 < pos1.Count - 1) {
-                c1 += 1;
-            }
-            else {
-                c1 = 0;
-            }
-        }
-        else if (buttonPressed == 1){
-            if (c2 < pos2.Count - 1) {
-                c2 += 1;
-            }
-            else {
-                c2 = 0;
-            }
-        }
-        else if (buttonPressed == 2){
-            if (c3 < pos3.Count - 1) {
-                c3 += 1;
-            }
-            else {
-                c3 = 0;
-                }
-        }
-        else if (buttonPressed == 3){
-            if (c4 < pos4.Count - 1) {
-                c4 += 1;
-            }
-            else {
-                c4 = 0;
-            }
-        }
-        else if (buttonPressed == 4){
-            if (c5 < pos5.Count - 1) {
-                c5 += 1;
-            }
-            else {
-                c5 = 0;
-            }
-        }
-        char1Text.text = pos1[c1].ToString();
-        char2Text.text = pos2[c2].ToString();
-        char3Text.text = pos3[c3].ToString();
-        char4Text.text = pos4[c4].ToString();
-        char5Text.text = pos5[c5].ToString();
+        currentIndices[buttonPressed] = (currentIndices[buttonPressed] + 1) % characters[buttonPressed].Length;
 
+        charDisplays[buttonPressed].text = characters[buttonPressed][currentIndices[buttonPressed]].ToString();
     }
+
     public void ReverseCycle(int buttonPressed)
     {
-        if (buttonPressed == 0){
-            if (c1 > 0) {
-                c1 -= 1;
-            }
-            else {
-                c1 = pos1.Count - 1;
-            }
-        }
-        else if (buttonPressed == 1){
-            if (c2 > 0) {
-                c2 -= 1;
-            }
-            else {
-                c2 = pos2.Count - 1;
-            }
-        }
-        else if (buttonPressed == 2){
-            if (c3 > 0) {
-                c3 -= 1;
-            }
-            else {
-                c3 = pos3.Count - 1;
-                }
-        }
-        else if (buttonPressed == 3){
-            if (c4 > 0) {
-                c4 -= 1;
-            }
-            else {
-                c4 = pos4.Count - 1;
-            }
-        }
-        else if (buttonPressed == 4){
-            if (c5 > 0) {
-                c5 -= 1;
-            }
-            else {
-                c5 = pos5.Count - 1;
-            }
-        }
-        char1Text.text = pos1[c1].ToString();
-        char2Text.text = pos2[c2].ToString();
-        char3Text.text = pos3[c3].ToString();
-        char4Text.text = pos4[c4].ToString();
-        char5Text.text = pos5[c5].ToString();
+        if (currentIndices[buttonPressed] == 0)
+            currentIndices[buttonPressed] = characters[buttonPressed].Length - 1;
+        else
+            currentIndices[buttonPressed] = (currentIndices[buttonPressed] - 1);
 
+        charDisplays[buttonPressed].text = characters[buttonPressed][currentIndices[buttonPressed]].ToString();
     }
 }
