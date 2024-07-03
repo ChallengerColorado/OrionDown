9e36ddeee51544276d58cc820b4b86d6295c8ad6
qazxwecvr <lucasdangeranderson@gmail.com>
2024-07-02 19:05:21 -0600

    Fix syntax errors in Keypad.cs

--- OrionDown/Assets/Scripts/Keypad.cs
+++ OrionDown/Assets/Scripts/Keypad.cs
@@ -2,25 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Keypad : MonoBehaviour
+public class Keypad : ModuleBehaviour
 {
 
     private System.Random random = new System.Random();
 
     private List<List<string>> allSymbols = new List<List<string>>{
-    private List<string> symbols1 = new List<string>(){"a", "b", "c", "d"};
-    private List<string> symbols2 = new List<string>(){"a", "b", "c", "e"};
-    private List<string> symbols3 = new List<string>(){"e", "b", "c", "d"};
-    private List<string> symbols4 = new List<string>(){"a", "e", "c", "d"};
-    private List<string> symbols5 = new List<string>(){"a", "b", "e", "d"};
-    }
-    private List<int> buttonIdicies = new List<int>;
+        new List<string>(){"a", "b", "c", "d"},
+        new List<string>(){"a", "b", "c", "e"},
+        new List<string>(){"e", "b", "c", "d"},
+        new List<string>(){"a", "e", "c", "d"},
+        new List<string>(){"a", "b", "e", "d"}
+    };
+    private List<int> buttonIndices = new List<int>();
     
-    private List<string> symbolsUsed = new List<string>;
+    private List<string> symbolsUsed = new List<string>();
 
     private int tempIndex = -1;
 
     private bool complete = false;
+
+    private int symbolIndex;
+    private List<string> symbols;
 
     // Start is called before the first frame update
     void Start()
@@ -31,31 +34,32 @@ public class Keypad : MonoBehaviour
 
     private void InitializeRound()
     {
-        private int symbolIndex = system.Next(allsymbols.Count)
-        private List<string> symbols = new List<string>(allSymbols[symbolIndex]);
+        symbolIndex = random.Next(allSymbols.Count);
+        symbols = new List<string>(allSymbols[symbolIndex]);
         
-        for (int i = 0; i i < 4; i++){
-            private int newindex = system.Next(symbols.Count);
+        for (int i = 0; i < 4; i++) {
+            int newindex = random.Next(symbols.Count);
             symbolsUsed.Add(symbols[newindex]);
             symbols.RemoveAt(newindex);
         }
     }
     private void ButtonPress(int buttonIndex) {
         buttonIndices.Add(buttonIndex);
-        if (buttonIndex.Count > 3 and complete = false;){
+        if (buttonIndices.Count > 3 && complete == false){
             CheckSolution();
         }
     }
 
     private void CheckSolution() {
         for (int i = 0; i <4; i++){
-            if (tempIndex > allSymbols[symbolIndex].BinarySearch(symbolsUsed[buttonIndices[i]]);){
+            if (tempIndex > allSymbols[symbolIndex].BinarySearch(symbolsUsed[buttonIndices[i]])){
                 tempIndex = -1;
                 buttonIndices.Clear();
                 return;
             }
                 tempIndex = allSymbols[symbolIndex].BinarySearch(symbolsUsed[buttonIndices[i]]);
         }
+
         SetStatus(true, "BB");
         complete = true;
         return;
